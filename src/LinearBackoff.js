5cbb6e2def7d30be851caf024783b2a9d0bd9467
kiran2s <kiranssivakumar@gmail.com>
2017-08-02 10:26:19 -0700

    begin using generator for nextDelay function

--- src/LinearBackoff.js
+++ src/LinearBackoff.js
@@ -3,13 +3,41 @@
 var Backoff = require('./Backoff');
 
 class LinearBackoff extends Backoff {
-    constructor(service, args, retryCondition, offset, initialDelay, maxRetries, maxDelay, syncTimeout = null, debug = false) {
-        let nextDelay = this.makeNextDelay(offset);
-        super(service, args, retryCondition, nextDelay, initialDelay, maxRetries, maxDelay, syncTimeout, debug);
+    constructor(service, args, retryCondition, offset, initialDelay, maxRetries, maxDelay, mod = null, syncTimeout = null, debug = false) {
+        super(service, args, retryCondition, null, initialDelay, maxRetries, maxDelay, syncTimeout, debug);
+        this.setOffset(offset)
+            .setMod(mod)
+            .setNextDelay(this.makeNextDelay(this.initialDelay, this.offset, this.mod));
+    }
+
+    setOffset(offset) {
+        this.offset = offset;
+        if (typeof offset === "undefined" || offset === null) {
+            this.offset = 100;
+        }
+        return this;
+    }
+
+    setMod(mod) {
+        this.mod = mod;
+        if (typeof mod === "undefined") {
+            this.mod = null;
+        }
+        return this;
     }
 
     makeNextDelay(offset) {
         return (delayAmt => { return delayAmt + offset });
+    }
+
+    makeNextDelay(initialDelay, offset, mod) {
+        let nextDelayGenerator = function*(initialDelay, offset) {
+
+        }
+
+        return function() {
+            
+        }
     }
 }
 
